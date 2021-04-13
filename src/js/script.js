4d47feacb58a908e799f06459f4f72d4a4af1557
Andrej Babiak <andrejbabiak@int.pl>
2021-04-13 20:29:16 +0200

    add images hide\show funct

--- src/js/script.js
+++ src/js/script.js
@@ -84,6 +84,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
     initAccordion() {
       const thisProduct = this;
@@ -133,14 +134,15 @@
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
 
         // for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          const optionImage = thisProduct.imageWrapper.querySelector(`.${paramId}-${optionId}`);
+          if(optionSelected) {
+
             // check if the option is not default
             if(option.default !== true) {
               // add option price to price variable
@@ -153,6 +155,14 @@
               price -= option.price;
             }
           }
+
+          if(optionImage) {
+            if(optionSelected) {
+              optionImage.classList.add('active');
+            } else if(!optionSelected) {
+              optionImage.classList.remove('active');
+            }
+          }  
         }
       }
 
